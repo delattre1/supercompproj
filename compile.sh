184a1c80d3965751fe19f7413abc34611e2b1da2
delattre1 <daniieeldelattre@gmail.com>
2022-12-05 23:53:33 -0300

    chore: added compile of exaustiva

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
-
+g++ -O3 -pedantic -std=c++11 busca-exaustiva/busca-exaustiva.cpp -o busca-exaustiva/busca_exaustiva &&
 g++ -O3 -Wall -pedantic -std=c++11 -fopenmp busca-local/busca_local.cpp -o busca-local/busca_local_openmp &&
 g++ -O3 -Wall -pedantic -std=c++11 busca-local/busca_local.cpp -o busca-local/busca_local
